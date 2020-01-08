b6df3ec04e7b8da9367247aeb724954841699c12
Gabriele <gabrivolpe95@gmail.com>
2020-01-08 11:45:20 +0100

    mynetclass

---
+++ mynet.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+from torch.hub import load_state_dict_from_url
+
+
+# = ['AlexNet', 'alexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+
+class MyNet(nn.Module):
+
+    def __init__(self, num_classes=7):
+        super(MyNet, self).__init__()
+        self.gf = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.gy = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        self.gd = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 2),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def mynet(progress=True, **kwargs):
+
+    model = MyNet(**kwargs)
+
+    state_dict = load_state_dict_from_url(model_urls['alexnet'], progress=progress)
+    model.load_state_dict(state_dict)
+    model.gd[1].weight.data = model.gy[1].weight.data
+    model.gd[1].bias.data = model.gy[1].bias.data
+    return model
